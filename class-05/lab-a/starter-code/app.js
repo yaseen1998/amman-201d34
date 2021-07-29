53ae4646cc66eb6dd9bb48dd16ba894fbed20685
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2021-07-29 16:47:23 +0300

    solve problem 4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -70,19 +70,19 @@ testSumAndMultiply(4,7,5);
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // // Write your code here
-// let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; //eslint-disable-line
 
-// function sumArray(sumArr) { //eslint-disable-line
-// let sum_array = 0
-//   for(let i = 0 ;i<sumArr.length;i++){
-//     sum_array += sumArr[i];
-//   }
-//   return [sum_array,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum_array} is their sum.`]
-// }
+function sumArray(sumArr) { //eslint-disable-line
+let sum_array = 0
+  for(let i = 0 ;i<sumArr.length;i++){
+    sum_array += sumArr[i];
+  }
+  return [sum_array,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum_array} is their sum.`]
+}
 
 // // Here is the test for sumArray(); uncomment it to run it
 
-// //testSumArray(testArray);
+testSumArray(testArray);
 
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
